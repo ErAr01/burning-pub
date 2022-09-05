8f717e231035fe2f999603bdce17e910b2b51626
aermilov <arermilov2001@gmail.com>
2022-09-05 13:13:18 +0300

    Работает, не точные рпс, кривой график

--- locustfile.py
+++ locustfile.py
@@ -1,17 +1,38 @@
-from ssl import RAND_pseudo_bytes
-from typing_extensions import Self
-from locust import HttpUser, task, constant_throughput
+from cProfile import run
+import math
+from locust import HttpUser, TaskSet, task, constant
+from locust import LoadTestShape
 
-class HelloWorldUser(HttpUser):
-    count = 1
-    rps = 1
-    wait_time = constant_throughput(rps)
-    # fixed_count = 5000
 
+class UserTasks(TaskSet):
     @task
-    def from_0_to_50(self):
-        while self.count <= 50:
-            self.rps += 1 
-            self.count += 1
-            self.client.get("/test1")
-            print(self.rps, self.count)
+    def get_root(self):
+        self.client.get("/test1")
+        # self.client.get("/test2")
+        # self.client.get("/test3")
+
+
+class WebsiteUser(HttpUser):
+    wait_time = constant(1)
+    tasks = [UserTasks]
+
+
+class DoubleWave(LoadTestShape):
+
+    def tick(self):
+        run_time = round(self.get_run_time())
+        user_count = 0
+        
+        if run_time < 60:
+            user_count = run_time
+            return (round(user_count), 1)
+        elif run_time < 120:
+            return (50, 1)
+        elif run_time < 180:
+            user_count = run_time -12
+            print(user_count)
+            return (round(user_count), 2)
+        elif run_time < 225:
+            return (1, 4)
+        else:
+            return None
